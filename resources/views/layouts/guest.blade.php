faf9db12897e99d5ab2721cdc0e7eeb28849b03a
amrullahdev <amrullahdev@gmail.com>
2021-06-27 05:06:42 +0700

    add navbar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,7 +1,31 @@
 <body>
-    <div class="container py-4" id="app">
-        <router-view></router-view>
-        <vue-progress-bar></vue-progress-bar>
+    <div class="pb-4" id="app">
+    <div class="p-2 bgf-dark text-white">
+            <div class="container">
+                <div class="row no-gutters">
+                    <div class="col-sm">
+                        <div>
+                            <router-link to="/" class="tdn text-white font-weight-bolder" exact-active-class="text-white">
+                                AMR Apps
+                            </router-link>
+                        </div>
+                    </div>
+                    <div class="col-sm text-right">
+                        <div>
+                            <router-link to="/login" class="tdn text-white" exact-active-class="text-white">
+                                Login
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="container py-4">
+            <router-view></router-view>
+            <vue-progress-bar></vue-progress-bar>
+
+        </div>
     </div>
     <script src="/js/app.js" defer></script>
 </body>
