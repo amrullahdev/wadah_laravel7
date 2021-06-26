d5536ce288e2cca27786ba14f9f3dd2f954c4589
amrullahdev <amrullahdev@gmail.com>
2021-06-27 04:17:01 +0700

    tambah extend cntroller  mengubah kode user menjadi numerik

--- app/Repositories/autentikasiRepos.php
+++ app/Repositories/autentikasiRepos.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
 
 namespace App\Repositories;
 
+use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Support\Facades\Hash;
 
-class autentikasiRepos
+class autentikasiRepos extends Controller
 {
 	public function registrasiAutentikasi($request)
 	{
-		$data=$request->all();
-		$data['kd_user'] = '001_'.substr(rand(0,99999)+time(), 0,10);
-		$data['password']=Hash::make($data['password']);
+		$data = $request->all();
+		$data['kd_user'] =  $this->get_primaryKey(101);
+		$data['password'] = Hash::make($data['password']);
 		User::create($data);
 	}
 }
