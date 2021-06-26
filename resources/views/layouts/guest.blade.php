92341ce34671a0410677c32da61ee280ace1a2fe
amrullahdev <amrullahdev@gmail.com>
2021-06-27 04:38:21 +0700

    new file

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,7 @@
+<body>
+    <div class="container py-4" id="app">
+        <router-view></router-view>
+        <vue-progress-bar></vue-progress-bar>
+    </div>
+    <script src="/js/app.js" defer></script>
+</body>
