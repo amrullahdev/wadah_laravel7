ea280cf8b7e39c6fda8556c201e147ee28d451de
amrullahdev <amrullahdev@gmail.com>
2021-07-13 09:22:24 +0700

    mindahin file

---
+++ resources/vue-views/Auth/loged_page.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Selamat Kita tela login dan di alihkan ke page ini
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods:{
+
+        },
+    }
+</script>
